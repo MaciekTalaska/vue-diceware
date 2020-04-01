96b7f1d8725c42de1ff8dd904918acb2008a679c
MaciekTalaska <maciek.talaska@gmail.com>
2020-04-01 20:49:10 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <diceware msg="Diceware Password generator in Vue"></diceware>
+  </div>
+</template>
+
+<script>
+import diceware from "./components/diceware";
+
+export default {
+  name: "App",
+  components: {
+    diceware
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/diceware.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Diceware password generator in Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "diceware",
+    props: {
+        msg: String
+    }
+};
+</script>
+
+<style scoped>
+</style>
