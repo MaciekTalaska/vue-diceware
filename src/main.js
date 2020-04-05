4eff1776f905c7cf1d567174fcf7db3e0760b8a9
MaciekTalaska <maciek.talaska@gmail.com>
2020-04-05 17:55:18 +0200

    minor changes

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App), 
+  el: '#root'
+});
+
+// original:
+// new Vue({
+//   render: h => h(App)
+// }).$mount("#root");
